555534bcc27b518c4aaac68b7664dae5567fb72d
Nasyarobby <nasyarobby@gmail.com>
2020-08-25 16:51:15 +0700

    use cors

--- Server.js
+++ Server.js
@@ -1,6 +1,12 @@
 const restify = require("restify");
 const server = restify.createServer();
 
+const cors = require("restify-cors-middleware")({
+  origins: ["*"],
+});
+server.pre(cors.preflight);
+server.use(cors.actual);
+
 const jsend = require("jsend");
 server.use(jsend.middleware);
 server.use(restify.plugins.bodyParser());
